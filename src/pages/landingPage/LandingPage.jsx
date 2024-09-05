f7b00339e4013de30ad819ebdf01e26f746ed632
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-09-05 18:55:29 +0530

    landing-page-feature-section-added

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -3,12 +3,14 @@ import "./LandingPage.css";
 import LandingNavbar from "../../components/specific/landingPage/landingNavbar/LandingNavbar";
 import LandingHome from "../../components/specific/landingPage/landingHome/LandingHome";
 import LandingAbout from "../../components/specific/landingPage/landingAbout/LandingAbout";
+import LandingFeatures from "../../components/specific/landingPage/landingFeatures/LandingFeatures";
 export default function LandingPage() {
   return (
     <div>
       <LandingNavbar />
       <LandingHome />
       <LandingAbout />
+      <LandingFeatures />
     </div>
   );
 }
