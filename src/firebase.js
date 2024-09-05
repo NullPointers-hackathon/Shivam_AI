762aa3000cf84c52a75f454b0a959af117b85b03
Mahesh-addagatla <maheshaddagatla1234@gmail.com>
2024-09-05 22:49:09 +0530

    layout built

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,6 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithPopup, FacebookAuthProvider,GoogleAuthProvider } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
