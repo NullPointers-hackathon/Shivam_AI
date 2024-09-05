efafe6faa996553acea679b60e87491610ac168b
Harikesh <harikeshav99@gmail.com>
2024-09-05 19:57:49 +0530

    just like that

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
-import OutputDisplay from "./components/common/outputdisplay/OutputDisplay";
 
 
 export default function App() {
@@ -10,7 +9,6 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/OutputDisplay" element={<OutputDisplay />} />
         </Routes>
       </Router>
     </>
