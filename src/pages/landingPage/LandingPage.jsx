0709e2b23f3beadb8014ccb237af9ff4e54a2b7b
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-09-05 18:31:46 +0530

    LandingPage LandingAbout

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./LandingPage.css";
 import LandingNavbar from "../../components/specific/landingPage/landingNavbar/LandingNavbar";
 import LandingHome from "../../components/specific/landingPage/landingHome/LandingHome";
+import LandingAbout from "../../components/specific/landingPage/landingAbout/LandingAbout";
 export default function LandingPage() {
   return (
     <div>
       <LandingNavbar />
       <LandingHome />
+      <LandingAbout />
     </div>
   );
 }
